089243907919218af3f396127ccd71fd0085015e
Cristian Felipe Gallego Ramírez <pharsat@hotmail.com>
2017-08-20 16:06:54 -0500

    Se finaliza la creación de la ruta.

--- app/src/main/java/com/android/cristiangallego/reportados/vistas/NuevoReporte.java
+++ app/src/main/java/com/android/cristiangallego/reportados/vistas/NuevoReporte.java
@@ -1,7 +1,9 @@
 package com.android.cristiangallego.reportados.vistas;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.android.cristiangallego.reportados.R;
 
@@ -11,5 +13,10 @@ public class NuevoReporte extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nuevo_reporte);
+    }
+
+    public void accionListaReportes(View v)
+    {
+        startActivity(new Intent(this, MisReportes.class));
     }
 }
